c25406de60bece5dfb7242bbd512239b45610c53
Abe Cede <abecede@gmail.com>
2021-02-01 15:21:52 +0200

    more consistent naming convention

--- races/management/commands/botfunctions.py
+++ races/management/commands/botfunctions.py
@@ -5,8 +5,10 @@ from discord.ext import commands
 
 from races.models import RaceSetup, RaceQueue
 
-description = '''Here are the commands to set up "race queues" for hosting a
-couple of races in order with friends.'''
+description = ('Here are the commands to set up "rounds" of car/track combos '
+               'for hosting a couple of races with friends.\nEach round '
+               'consists of 10min qualifying, 10min race one, 10min race two '
+               'with inverted starting grid.')
 
 intents = discord.Intents.default()
 intents.members = True
@@ -17,10 +19,13 @@ bot = commands.Bot(command_prefix='', description=description,
 
 @bot.command()
 async def list(ctx):
-    """Show a list of all race-setups."""
+    """Shows all car/track combos
+
+    You can then add a combo (using its ID) to the queue for having fun with your friends.
+    """
     MAX_LEN = 40
     embed = discord.Embed()
-    embed.title = "These are all race setups for now."
+    embed.title = "These are all combos for now."
     description = ("`  ID` `{0:" + str(MAX_LEN) + "}`\n").format('Title')
     rslist = []
     for rs in RaceSetup.objects.all().order_by('title'):
@@ -32,9 +37,9 @@ async def list(ctx):
             id=rs.id, title=title))
     description += '\n'.join(rslist)
     embed.description = description
-    embed.set_footer(text='Add a race setup to the queue with '
+    embed.set_footer(text='Add a combo to the queue with '
                      '`append ID` or `insert ID`.\n'
-                     'For more information about a race setup use `info ID`.')
+                     'For more information about a combo use `info ID`.')
     await ctx.send(embed=embed)
 
 
@@ -50,7 +55,12 @@ def _list_queue(objects=False):
 
 @bot.command()
 async def queue(ctx):
-    """Show the current race queue."""
+    """Shows the current queue of rounds
+
+    When you or someone else "appends" or "inserts" a combo, then this will be
+    added as a round to the queue. Each round usually consists of qualifying, race 1
+    and race 2 (with reversed grid).
+    """
     await ctx.send(embed=_queueembed())
 
 
@@ -58,7 +68,7 @@ def _queueembed():
     MAX_LEN = 32
     result = _list_queue(objects=True)
     embed = discord.Embed()
-    embed.title = '**Upcoming race-setups:**'
+    embed.title = '**Upcoming combos:**'
     if result:
         rslist = []
         description = ("` ID` `{0:" + str(MAX_LEN) + "}`").format('Title') + \
@@ -84,22 +94,28 @@ def _queueembed():
         description += '\n'.join(rslist)
         embed.description = description
         embed.set_footer(
-            text='For more information about a race use `info ID`.')
+            text='For more information about a combo use `info ID`.')
     else:
         embed = discord.Embed(color=0x00ff00)
         embed.title = 'The queue is empty. ' + \
-            'Therefore a random race setup will be used next.'
+            'Therefore a random combo will be used for the next round.'
     return embed
 
 
 @bot.command()
 async def append(ctx, id: int):
-    """Append a race-setup to the end of the race queue."""
+    """Appends a combo to the end of the queue
+    
+    Puts the combo with the specified id (as a number which you can find out
+    using the 'list' command) to the end of the queue.
+
+    Example: append 28
+    """
     try:
         rs = RaceSetup.objects.get(pk=id)
     except Exception:
         await ctx.send(embed=error(
-            "Sorry, I could not find a race setup with that ID."))
+            "Sorry, I could not find a combo with that ID."))
         return
     queue_length = RaceQueue.objects.all().count()
     max_index = 0
@@ -108,18 +124,25 @@ async def append(ctx, id: int):
     rq = RaceQueue(setup=rs, index=max_index + 1)
     rq.save()
     embed = _queueembed()
-    embed.title = 'The race setup has been appended. Here is the current queue.'
+    embed.title = 'The combo has been appended. Here is the current queue.'
     await ctx.send(embed=embed)
 
 
 @bot.command()
 async def insert(ctx, id: int):
-    """Insert a race-setup at the start of the race queue."""
+    """Inserts a combo at the beginning of the queue
+    
+    Puts the combo with the specified id (as a number which you can find out
+    using the 'list' command) to the start of the queue, so it will be used
+    right after the currently running round.
+
+    Example: insert 28
+    """
     try:
         rs = RaceSetup.objects.get(pk=id)
     except Exception:
         await ctx.send(embed=error(
-            "Sorry, I could not find a race setup with that ID."))
+            "Sorry, I could not find a combo with that ID."))
         return
     queue_length = RaceQueue.objects.all().count()
     new_index = 1
@@ -130,35 +153,46 @@ async def insert(ctx, id: int):
     rq = RaceQueue(setup=rs, index=new_index)
     rq.save()
     embed = _queueembed()
-    embed.title = 'The race setup has been inserted. Here is the current queue.'
+    embed.title = 'The combo has been inserted. Here is the current queue.'
     await ctx.send(embed=embed)
 
 
 @bot.command()
 async def clear(ctx):
-    """Clear the race queue."""
+    """Clears the queue
+    
+    Deletes all rounds waiting in the queue. The currently running round
+    continues until finished, and after that a new round with a
+    random combo will be started. (As long as the queue stays empty)
+    """
     RaceQueue.objects.all().delete()
     embed = discord.Embed(color=0x00ff00)
-    embed.title = "The queue has been emptied. " + \
-        "A random race setup will be used next."
+    embed.title = "The queue has been emptied."
     await ctx.send(embed=embed)
     return
 
 
 @bot.command()
 async def next(ctx):
-    """Abort current race and start the next one."""
+    """Immediately starts next round
+
+    Aborts the current round and immediately starts the next one in the
+    queue, if there are combos waiting. Otherwise starts a random combo."""
     subprocess.run("sudo /usr/bin/systemctl restart acserver.service".split())
     embed = discord.Embed(color=0x00ff00)
-    embed.title = "The current race has been aborted and the next one started."
+    embed.title = "The current round has been aborted and the next one started."
     await ctx.send(embed=embed)
     return
 
 
 @bot.command()
 async def playlist(ctx, *, ids: str):
-    """Immediately start a playlist with the tracks given by id.
-    Example: `playlist 14 15 16`"""
+    """Immediately starts a playlist
+    
+    Aborts the current round, clears the queue, and adds the combos given by id
+    to the queue. Useful when starting a playlist with friends.
+
+    Example: playlist 14 15 16"""
     try:
         idlist = [int(x) for x in ids.split(' ')]
     except Exception:
@@ -190,13 +224,14 @@ async def playlist(ctx, *, ids: str):
 
 @bot.command()
 async def info(ctx, id: int):
-    """Get more information about a race setup. use 'info ID', where ID is the
-    number you get when you 'list' all race setups."""
+    """Get more information about a combo.
+    
+    Use 'info ID', where ID is the number you get when you 'list' all race setups."""
     try:
         rs = RaceSetup.objects.get(pk=id)
     except Exception:
         await ctx.send(embed=error(
-            "Sorry, I could not find a race setup with that ID."))
+            "Sorry, I could not find a combo with that ID."))
         return
     embed = discord.Embed()
     embed.title = rs.title
@@ -218,20 +253,20 @@ def error(description):
 
 
 # STUFF FOR NEXT IMPLEMENTATIONS (roles and such)
-@bot.command()
-async def dbg(ctx, x: str):
-    """Clear the race queue."""
-    embed = discord.Embed(color=0x00ff00)
-    embed.title = "In debug mode..."
-    await ctx.send(embed=embed)
-    """
-ctx.message.channel.name
-'now-playing'
-
-[x.name for x in ctx.author.roles]
-['@everyone', 'Member', 'Early Bird']
-
-    """
-    embed = discord.Embed(color=0x00ff00)
-    embed.title = "Continuing."
-    await ctx.send(embed=embed)
+# @bot.command()
+# async def dbg(ctx, x: str):
+#     """Clear the race queue."""
+#     embed = discord.Embed(color=0x00ff00)
+#     embed.title = "In debug mode..."
+#     await ctx.send(embed=embed)
+#     """
+# ctx.message.channel.name
+# 'now-playing'
+# 
+# [x.name for x in ctx.author.roles]
+# ['@everyone', 'Member', 'Early Bird']
+# 
+#     """
+#     embed = discord.Embed(color=0x00ff00)
+#     embed.title = "Continuing."
+#     await ctx.send(embed=embed)
