53de5a61fdb9fcf18bde607054593ce7e3640f0f
Abe Cede <abecede@gmail.com>
2021-06-01 17:42:49 +0300

    various.

--- tracks/models.py
+++ tracks/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Track(models.Model):
     title = models.CharField(max_length=128, unique=True)
     slug = models.CharField(max_length=128, unique=True)
+    layout = models.CharField(max_length=128, default='')
     rsr_id = models.IntegerField(default=0)
     dlc_slug = models.CharField(max_length=128, blank=True, default='')
     download_url = models.URLField(max_length=2048, blank=True, default='')
